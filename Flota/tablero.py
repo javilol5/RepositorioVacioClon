3cab17dde3e3ff89aa8d408deb5f297bc1bbd7ad
javilol5 <javierlolcasal@gmail.com>
2025-11-25 14:14:10 +0100

    feat(tablero.py): creacion de funcion mostrar_tablero

--- Flota/tablero.py
+++ Flota/tablero.py
@@ -10,3 +10,9 @@ class Tablero:
             for columna in range(tamano):
                 fila_actual.append(0)
             self.casillas.append(fila_actual)
+
+    def mostrar_tablero(self):
+
+        print("\nTABLERO:")
+        for fila in self.casillas:
+            print(" ".join(str(casilla) for casilla in fila))
